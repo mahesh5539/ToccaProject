c3652baffca83719787960aa074cc0ca93d8619d
mahesh_connexus <mahesh.dhamgunde@connexussecure.com>
2019-04-04 20:03:45 -0400

    modified /user-profiles api response

--- routes/index.js
+++ routes/index.js
@@ -17,19 +17,36 @@ router.get('/user-profile', function(req, res, next) {
 
 router.get('/user-profiles', function(req, res, next) {
 
-  let limit = req.query.limit;
-  let startAt = req.query.startAt;
+  var limit = req.query.limit;
+  var startAt = req.query.nextPageIndex;
+  var result = {};
 
   if(!limit){
     limit = 51;
+  }else{
+    limit = Number(limit) + 1;
   }
+
   if(!startAt){
     startAt = 1;
   }
 
-  firebase.database().ref('/user_profile').orderByChild('timestamp').startAt(startAt).limitToFirst(Number(limit)).once('value').then(function(snapshot) {
+  firebase.database().ref('/user_profile').orderByChild('timestamp').startAt(startAt).limitToFirst(limit).once('value').then(function(snapshot) {
     var data = snapshot.val();
-    res.send(data);
+    var returnArr = [];
+
+    snapshot.forEach(function(childSnapshot) {
+        var item = childSnapshot.val();
+        item.key = childSnapshot.key;
+        returnArr.push(item);
+    });
+    var nextIndex = returnArr.pop().timestamp;
+    
+    result.nextPageIndex = index;
+    result.nextPageUrl = "/user-profiles?limit=50&nextPageIndex=" + nextIndex;
+    result.userProfiles = returnArr;
+
+    res.send(result);
   })
 });
 
